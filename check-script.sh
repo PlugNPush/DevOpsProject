120614d71d46a197fc224def5345f1921bbb4340
RomainMarques <77971683+RomainMarques@users.noreply.github.com>
2023-07-19 14:33:36 +0200

    fix pipeline

--- check-script.sh
+++ check-script.sh
@@ -14,7 +14,7 @@ assert_site_responding() {
   fi
 }
 
-url="https://plugnpush.github.io/DevOpsProject/t"
+url="https://plugnpush.github.io/DevOpsProject/"
 expected_status_code=200
 
 assert_site_responding "$url" "$expected_status_code"
